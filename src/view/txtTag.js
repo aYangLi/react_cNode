94394a2ed1ee1f7f3204788543089579cea28d79
liChenYang <chenyang.li@dmall.com>
2019-03-24 16:54:16 +0800

    tag 封装完成

---
+++ src/view/txtTag.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {Tag} from 'antd';
+
+const tag = {
+  top: {
+    color:'magenta',
+    txt:'置顶',
+  },
+  good : {
+    color:'geekblue',
+    txt:'精华',
+  },
+  job: {
+    color:'cyan',
+    txt:'招聘',
+  },
+  share: {
+    color:'purple',
+    txt:'分享',
+  },
+  ask: {
+    color:'green',
+    txt:'问答',
+  },
+  dev: {
+    color:'lime',
+    txt:'测试',
+  },
+};
+
+function getTab(data) {
+  return (
+    data.top?'top':
+      data.good?'good':data.tab
+  )
+}
+export default class TxtTag extends Component {
+  render() {
+    // console.log(getTab(this.props.data));
+    let nowTab = tag[ getTab(this.props.data) ];
+    return <Tag color={nowTab.color}>{nowTab.txt}</Tag>
+  }
+}
