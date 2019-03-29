7057fb84e2cba5b718ca64856f281c4e45c28cb7
liChenYang <chenyang.li@dmall.com>
2019-03-29 10:40:56 +0800

    首页导航默认选中状态；

--- src/view/nav.js
+++ src/view/nav.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 import {Menu, Icon,} from 'antd';
 
-export default class Nav extends Component {
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    let now = this.getNow(this.props.location);
+    this.state = {
+      now,
+    }
+
+  }
+  getNow (location) {
+    const now = location.pathname.split('/');
+    return now[1];
+  };
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    let now = this.getNow(nextProps.location);
+    if (now !== this.state.now) {
+      this.setState({
+        now,
+      });
+      return false;
+    }
+    return  true;
+  }
+
   render() {
     let {mode, id} = this.props; 
-    return (<Menu mode={mode} id={id} theme='light'>
-    <Menu.Item>
+    return (<Menu mode={mode} id={id} theme='light' selectedKeys={[this.state.now]}>
+    <Menu.Item key='index'>
       <Link to='/index/all'><Icon type='home'/>首页</Link>
     </Menu.Item>
-    <Menu.Item>
+    <Menu.Item key='book'>
       <Link to='/book'><Icon type='book'/>教程</Link>
     </Menu.Item>
-    <Menu.Item>
+    <Menu.Item key='about'>
       <Link to='/about'><Icon type='info-circle-o'/>关于</Link>
     </Menu.Item>
   </Menu>);
   }
 }
+
+export default withRouter( (props) => {
+  let {mode,id, location} = props;
+  return <Nav
+    mode = {mode}
+    id={id}
+    location = {location}
+  />
+})
